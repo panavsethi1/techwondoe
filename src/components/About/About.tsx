518503dec0c19a1b52f4057b7be4ed346f3c0f8f
panavsethi1 <panav.sethi.delhi@gmail.com>
2022-08-13 05:27:57 +0530

    fix: fixed explicit any errors

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useEffect, useState } from 'react';
 import useContentful from '../../hooks/useContentful';
 import AboutCard from './AboutCard';
